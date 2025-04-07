b235841416a2d87fbd885a14bd54de573f6e65b7
Sunanya02 <bhartiysunanya23@gmail.com>
2025-04-07 21:56:50 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,45 +1,17 @@
 import random
-'''
-1 for snake 
--1 for water 
-0 for gun
-'''
 
-computer=random.choice([-1,0,1])
+n=random.randint(1,100)
+a=-1
+guesses=0
 
-youstr=input("Enter your choice:")
-youDict =  {"snake":1, "water":-1, "gun":0}
-reverseDict={1:"snake", -1:"water", 0:"gun" }
-
-you = youDict[youstr]
-
-print(f"you chose {reverseDict[you]}\ncomputer chose {reverseDict[computer]}")
-
-if(computer==you):
-   print("It's Draw!")
-   
-else:
-
- if(computer==-1  and  you==1):
-    print("you won!")
-
- elif(computer==-1  and  you==0):
-    print("you Loss!")
-
- elif(computer==1  and  you==-1):
-    print("you Loss!")
-
- elif(computer==1  and  you==0):
-    print("you won!")
-
- elif(computer==0  and  you==-1):
-    print("you won!")
-
- elif(computer==0  and  you==1):
-    print("you Loss!")
-
- else:
-    print("Something went wrong!")
-
+while(a!=n):
+    a=int(input("Guess the number:"))
+    if(a>n):
+        print("Lower number please")
+        guesses+=1
+    elif(a<n):
+        print("Higher number please")
+        guesses+=1
     
-    
+
+print(f"You have guessed the number {n} correctly in guesses {guesses} attempts")
